511e3f06ef9b133cdf35e19056d467dbdebde72b
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2024-04-04 12:33:42 +0800

    step 6 meningkatkan jumlah perbandingan

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -41,8 +41,18 @@ void linearSearch()
 
         ctr = 0;
         i = 0; //step 2
-
+        while (i < n) //step 3
+        {
+            ctr++;
+            if (arr[i] == item)
+            {
+                cout << "\n " << item << " found at position " << (i + 1)
+                    << endl; //step 6
+                break;
+            }
+        }
     }
+
 }
 
 int main()
