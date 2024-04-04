7ad08f2c564009855a8fa3a446bfa65df0cea5b8
ZahwaLadyaMaharani <zahwa.ladya.ft20@mail.umy.ac.id>
2024-04-04 12:38:33 +0800

    step 5 memeriksa variabel mencapai jumlah elemen array serta menampilkan jumlah perbandingan

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -53,9 +53,14 @@ void linearSearch()
             i++; //step 4
         }
 
-    }
+        if (i == n) //step 5
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons: " << ctr << endl;
 
+        cout << "nContinue search (y/n): ";
+        cin >> ch;
 
+    } while ((ch == 'y') || (ch == 'Y'));
 }
 
 int main()
